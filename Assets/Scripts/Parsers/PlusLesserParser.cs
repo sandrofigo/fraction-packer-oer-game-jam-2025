5cc2b4bd52ebb8c8211c8c2c0a254f0634f362a5
paulp1412 <paul.preiner98@gmail.com>
2025-05-10 18:08:21 +0200

    update/add parsers

--- Assets/Scripts/Parsers/PlusLesserParser.cs
+++ Assets/Scripts/Parsers/PlusLesserParser.cs
@@ -1,25 +1,27 @@
 using System.Collections.Generic;
 
-public class AscParser : IParser
+public class PlusLesserParser
 {
     // examples
-    // n,n;n,n;n,n       => NULL/NULL < NULL/NULL < NULL/NULL
-    // n,2;n,n;n,n       => NULL/2    < NULL/NULL < NULL/NULL
-    // 1,2;5,n;n,6       =>    1/2    <    5/NULL < NULL/6
+    // n,n;n,n;n,n       => NULL/NULL + NULL/NULL < NULL/NULL
+    // n,2;n,n;n,n       => NULL/2    + NULL/NULL < NULL/NULL
+    // 1,2;5,n;n,6       =>    1/2    +    5/NULL < NULL/6
     public bool TryParse(string genDataStr, ref ParseData parseData)
     {
         string[] fractionStrings = genDataStr.Split(ParseUtils.FRACTION_SERPARATOR);
         parseData.amountFractions = fractionStrings.Length;
 
-        if (parseData.amountFractions < 2)
+        if (parseData.amountFractions < 3)
             return false;
 
         if (!ParseUtils.TryParseFractions(fractionStrings, ref parseData))
             return false;
 
         parseData.operators = new List<OperatorType>();
-        for (int i = 0; i < (parseData.amountFractions - 1); i++)
-            parseData.operators.Add(OperatorType.Less);
+        for (int i = 0; i < (parseData.amountFractions - 2); i++)
+            parseData.operators.Add(OperatorType.Plus);
+
+        parseData.operators.Add(OperatorType.Less);
 
         return true;
     }
