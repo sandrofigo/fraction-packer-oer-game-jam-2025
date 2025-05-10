80c550aaad7b232b4af87ed9ac4c764f5771f14d
Sandro Figo <sandro.figo@gmx.at>
2025-05-10 11:01:43 +0200

    Add basic stuff

---
+++ Assets/Scripts/Installers/GlobalInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Installers
+{
+    public class GlobalInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Scripts/Installers/SceneInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace Installers
+{
+    public class SceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
