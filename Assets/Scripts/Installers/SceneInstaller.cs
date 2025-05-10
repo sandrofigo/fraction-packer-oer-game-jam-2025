2e93d320cacdfafe62f640f5723e1e6189168a9e
Iris Trummer <iris.trummer@aon.at>
2025-05-10 17:48:04 +0200

    Create interactor through scene installer

--- Assets/Scripts/Installers/SceneInstaller.cs
+++ Assets/Scripts/Installers/SceneInstaller.cs
@@ -1,13 +1,19 @@
+using Interaction;
 using UI;
+using UnityEngine;
 using Zenject;
 
 namespace Installers
 {
     public class SceneInstaller : MonoInstaller
     {
+        [SerializeField]
+        private FractionBlockInteractor _fractionBlockInteractor;
+        
         public override void InstallBindings()
         {
             Container.Bind<SlotGroupComponent>().FromComponentInHierarchy().AsSingle().NonLazy();
+            Container.Bind<FractionBlockInteractor>().FromNewComponentOnNewPrefab(_fractionBlockInteractor).AsSingle().NonLazy();
         }
     }
 }
