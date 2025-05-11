645ab42005a752999c8f00c826858491049d429e
Iris Trummer <iris.trummer@aon.at>
2025-05-11 09:29:08 +0200

    Adjust spawned fraction block positions

--- Assets/Scripts/Fractions/FractionBuilder.cs
+++ Assets/Scripts/Fractions/FractionBuilder.cs
@@ -18,12 +18,31 @@ namespace Fractions
         [SerializeField]
         private FractionSlot _fractionSlotPrefab;
 
+        [SerializeField]
+        private Vector3 _animationStartPosition;
+
+        [SerializeField]
+        private Vector3 _spawnStartPosition;
+
+        [SerializeField]
+        private Vector3 _maxRandomOffset = new(0.25f, 0.25f, 0f);
+
+        [SerializeField]
+        private float _maxRandomRotationAngle = 30f;
+
+        [SerializeField]
+        private float _gap = 0.5f;
+
+        private int _spawnAmount;
+
         public FullFractionBlock CreateFullFraction(int numerator, int denominator)
         {
             FullFractionBlock block = _container.InstantiatePrefabForComponent<FullFractionBlock>(_fullFractionPrefab);
 
             block.NumeratorFractionComponent.SetValue(numerator);
             block.DenominatorFractionComponent.SetValue(denominator);
+
+            SetupFraction(block);
 
             return block;
         }
@@ -34,14 +53,37 @@ namespace Fractions
 
             block.FractionComponent.SetValue(value);
 
+            SetupFraction(block);
+
             return block;
         }
 
         public void CreateSlot(Vector3 position)
         {
             FractionSlot slot = _container.InstantiatePrefabForComponent<FractionSlot>(_fractionSlotPrefab);
-            
+
             slot.transform.position = position;
+        }
+
+        private void SetupFraction(AFractionBlock block)
+        {
+            block.transform.position = _animationStartPosition;
+            block.MoveTo(GetTargetPosition(), true);
+            
+            block.transform.Rotate(Vector3.up, Random.Range(-_maxRandomRotationAngle, _maxRandomRotationAngle));
+
+            _spawnAmount++;
+        }
+
+        private Vector3 GetTargetPosition()
+        {
+            float width = _partialFractionPrefab.GetComponentInChildren<MeshRenderer>().bounds.size.x;
+
+            Vector3 centerTargetPosition = _spawnStartPosition + Vector3.right * (width * _spawnAmount + _gap * _spawnAmount);
+
+            Vector3 randomOffset = new Vector3(Random.Range(0, _maxRandomOffset.x), 0, Random.Range(-_maxRandomOffset.z, _maxRandomOffset.z));
+
+            return centerTargetPosition + randomOffset;
         }
     }
 }
